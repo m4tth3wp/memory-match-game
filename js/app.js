6c164493735a76353dda41ae36387da885e8bd71
Matt <matt-paul@live.com>
2020-09-26 16:46:32 -0400

    Updates: -added constant variables -cached the button elements -added event listeners to the button elements

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
 console.log('app.js is linked!')
+
+/*----- constants -----*/
+const easyLevel = []
+const mediumLevel = []
+const hardLevel = []
+
+/*----- app's state (variables) -----*/
+let userProfile = {
+
+}
+/*----- cached element references -----*/
+let messageBox = document.querySelector('.message-box')
+let easyButton = document.getElementById('easy')
+let mediumButton = document.getElementById('medium')
+let hardButton = document.getElementById('hard')
+let startButton = document.getElementById('start')
+let resetButton = document.getElementById('reset')
+
+/*----- event listeners -----*/
+easyButton.addEventListener('click', function(e) {
+    console.log('easy button')
+})
+
+mediumButton.addEventListener('click', function(e) {
+    console.log('medium button')
+})
+
+hardButton.addEventListener('click', function(e) {
+    console.log('hard button')
+})
+
+startButton.addEventListener('click', function(e) {
+    console.log('start button')
+})
+
+resetButton.addEventListener('click', function() {
+    console.log('reset button')
+})
+/*----- functions -----*/
