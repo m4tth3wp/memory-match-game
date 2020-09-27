f81a76f3f45d0c64e3b3450584f2d24c5bb0827c
Matt <matt-paul@live.com>
2020-09-27 09:06:41 -0400

    Update Decided to structure the code using a class constructor, each difficulty will be a new object created from the class -created MemoryGame class -added the properties to it -created new objects using it for each difficulty -updated the event listeners

--- js/app.js
+++ js/app.js
@@ -1,14 +1,3 @@
-console.log('app.js is linked!')
-
-/*----- constants -----*/
-const easyLevel = []
-const mediumLevel = []
-const hardLevel = []
-
-/*----- app's state (variables) -----*/
-let userProfile = {
-
-}
 /*----- cached element references -----*/
 let messageBox = document.querySelector('.message-box')
 let easyButton = document.getElementById('easy')
@@ -16,25 +5,77 @@ let mediumButton = document.getElementById('medium')
 let hardButton = document.getElementById('hard')
 let startButton = document.getElementById('start')
 let resetButton = document.getElementById('reset')
+let gameBoard = document.querySelector('.game-board')
+let gameScore = document.querySelector('.score')
+
+/*----- constants -----*/
+class MemoryGame {
+    constructor(totalTime, difficulty) {
+        this.totalTime = totalTime
+        this.difficulty = difficulty
+        this.cardsRendered = 0
+        this.choices = []
+    }
+    resetGame() {
+        gameBoard.innerHTML = ''
+        gameScore.innerHTML = 0
+    }
+    cardAmount() {
+        let cards = this.difficulty
+        if(cards === 'easy') {
+            this.cardsRendered = 8
+        } else if (cards === 'medium') (
+            this.cardsRendered = 16
+        )
+        else {
+            this.cardsRendered = 24
+        } 
+    }
+    boardRender() {
+        for (let i = 0; i < this.cardsRendered; i++) {
+        // TODO: have this function populate the board with the choices
+        let gameGrid = document.createElement('div')
+        let gamePicks = document.createElement('img')
+        // gamePicks.src = '/img/IMG_6302.JPG'
+        gameGrid.appendChild(gamePicks)
+        gameBoard.appendChild(gameGrid)
+        }
+    }
+
+}
+/*----- app's state (variables) -----*/
+let easyGame = new MemoryGame(30, 'easy')
+let mediumGame = new MemoryGame(60, 'medium')
+let hardGame = new MemoryGame(120, 'hard')
 
 /*----- event listeners -----*/
-easyButton.addEventListener('click', function(e) {
-    console.log('easy button')
+easyButton.addEventListener('click', function() {
+    
+    easyGame.cardAmount()
+    easyGame.boardRender()
+    console.log(easyGame.cardsRendered)
 })
 
 mediumButton.addEventListener('click', function(e) {
-    console.log('medium button')
+    mediumGame.cardAmount()
+    mediumGame.boardRender()
+    console.log(mediumGame.cardsRendered)
 })
 
 hardButton.addEventListener('click', function(e) {
-    console.log('hard button')
+    hardGame.cardAmount()
+    hardGame.boardRender()
+    console.log(hardGame.cardsRendered)
 })
 
 startButton.addEventListener('click', function(e) {
     console.log('start button')
 })
 
-resetButton.addEventListener('click', function() {
-    console.log('reset button')
-})
+resetButton.addEventListener('click', resetGame)
+
 /*----- functions -----*/
+function resetGame() {
+    gameBoard.innerHTML = ''
+    gameScore.innerHTML = 0
+}
